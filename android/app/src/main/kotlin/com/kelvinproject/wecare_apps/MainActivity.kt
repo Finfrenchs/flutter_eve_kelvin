98f3bda3eea0ed2cdbc724dfd1aeec5db4d7f275
M. Kelvin Madrianto F <muhammadkelvinmf@gmail.com>
2022-11-26 23:36:48 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/kelvinproject/wecare_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kelvinproject.wecare_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+import 'package:wecare_apps/app/modules/home/bindings/home_binding.dart';
+import 'package:wecare_apps/app/modules/home/views/home_view.dart';
+import 'package:wecare_apps/app/modules/splashscreen/bindings/splashscreen_binding.dart';
+import 'package:wecare_apps/app/modules/splashscreen/views/splashscreen_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASHSCREEN,
+      page: () => SplashscreenView(),
+      binding: SplashscreenBinding(),
+    ),
+  ];
+}
